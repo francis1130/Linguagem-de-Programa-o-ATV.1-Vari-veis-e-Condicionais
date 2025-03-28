78ca858c6a7b21e9ac1f4fb596feefae542b2300
francis1130 <gabriel.cavalcanti23@etepd.com>
2025-03-28 19:58:17 -0300

    Create atv11.py

---
+++ atv11.py
@@ -0,0 +1,6 @@
+User = input("Digite o nome do usuário: ")
+Senha = input("Digite sua senha: ")
+if User == Senha: 
+     print("Erro, o número do usuário não pode ser igual a senha!")
+else: 
+     print("Acesso liberado")
